34fac483fb924be660fdfc96a8b27a8f82d99cc3
Elliot Barkis <e.cbarkis@gmail.com>
2017-03-12 16:04:33 -0700

    Initial commit finished assignemtOne

---
+++ AssignmentOne/assignmentOne.py
@@ -0,0 +1,23 @@
+name = 'Barkis'
+
+print name
+
+name = 'Elliot Barkis'
+
+print name
+
+print '5 + 8 = 13'
+
+string1 = 'Hello'
+
+string2 = 'World'
+
+print string1
+
+print string2
+
+print string1 + ' ' + string2
+
+num = 1
+
+print string1 + ' ' + string2 + ' ' + str(num)
