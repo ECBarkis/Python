fd0e755acc5d08c5a760aaa254fdec037f8fed4c
Elliot Barkis <e.cbarkis@gmail.com>
2017-03-12 23:21:00 -0700

    Finished AssignmentFour

---
+++ AssignmentThree/multiples.py
@@ -0,0 +1,16 @@
+for x in xrange( 1, 1000000, 2 ):
+	# print x
+	pass
+
+for x in xrange( 5, 1000000, 5 ):
+	# print x
+	pass
+
+a = [1, 2, 5, 10, 255, 3]
+
+sum = 0
+for element in a:
+	sum += element
+# print sum
+
+# print sum/len(a)
