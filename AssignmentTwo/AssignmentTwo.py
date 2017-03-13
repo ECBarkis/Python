2743aaa162e0ebfb607bc1f776ad6c46a07a3120
Elliot Barkis <e.cbarkis@gmail.com>
2017-03-12 17:54:14 -0700

    Finished AssignmentTwo

---
+++ AssignmentTwo/AssignmentTwo.py
@@ -0,0 +1,35 @@
+x = "If monkeys like bananas, then I must be a monkey!"
+
+# print x
+
+# print x.find('monkey')
+
+y = x.replace('monkey', 'alligator')
+
+# print y
+
+x = [2,54,-2,7,12,98]
+
+# print max(x)
+# print min(x)
+
+x = ["hello",2,54,-2,7,12,98,"world"]
+
+# print x[0]
+# print x[len(x)-1]
+
+x = [19,2,54,-2,7,12,98,32,10,-3,6]
+
+# print x
+
+x.sort()
+
+# print x
+
+y = x[5:]
+print y
+x = x[:5]
+print x
+y.insert(0, x)
+
+print y
